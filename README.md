19b24b019ba8462bc51b7ec4b7ba117aa3da0435
junx8 <junchen0102@gmail.com>
2025-03-31 10:54:16 +0800

    add benchmark script
    Signed-off-by: junx8 <junchen0102@gmail.com>

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
 # ov_pipeline_benchmark
+
+## Test for the anomalib-patchcore-resnet18 model with mvtecad-bottle
+
+### Test with CPU
+
+```
+python throughput_benchmark.py
+``` 
+
+```
+INFO:root:OpenVINO:
+INFO:root:Build ................................. 2025.0.0-17942-1f68be9f594-releases/2025/0
+
+Starting Performance Testing...
+
+Performance:
+ - 83 images task 2.43 sec, 0.029 sec/image
+ - throughput 34.15 FPS
+
+Saving the results...
+
+Saving Results Performance:
+ - 83 images task 8.36 sec, 0.101 sec/image
+ - throughput 9.93 FPS
+```
+
+
+### Test with GPU
+
+```
+python throughput_benchmark.py -d GPU
+```
+
+```
+INFO:root:OpenVINO:
+INFO:root:Build ................................. 2025.0.0-17942-1f68be9f594-releases/2025/0
+
+Starting Performance Testing...
+
+Performance:
+ - 83 images task 1.98 sec, 0.024 sec/image
+ - throughput 41.95 FPS
+
+Saving the results...
+
+Saving Results Performance:
+ - 83 images task 5.00 sec, 0.060 sec/image
+ - throughput 16.59 FPS
+```
---
+++ throughput_benchmark.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Copyright (C) 2022 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+
+import logging as log
+import sys
+import statistics
+from time import perf_counter
+import os
+import numpy as np
+import openvino as ov
+from openvino.utils.types import get_dtype
+import torch
+from anomalib.visualization.image.item_visualizer import visualize_image_item
+from anomalib.data.datasets import MVTecADDataset
+import torchvision.transforms as transforms
+import time
+import argparse
+
+
+def arg_parser():
+    parser = argparse.ArgumentParser(description='Performance test for model inference pipeline', formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument('-m', '--model', type=str, default="ovmodels/patchcore_resnet18_bottle/model.xml", help='the OpenVINO IR *.xml')
+    parser.add_argument('-d', '--device', default='CPU', help=f'The device to do inference')
+
+    return parser.parse_args()
+
+def completion_callback(infer_request: ov.InferRequest, data) -> None:
+    pred_score = infer_request.get_tensor("pred_score").data
+    anomaly_map = infer_request.get_tensor("anomaly_map").data
+    pred_mask = infer_request.get_tensor("pred_mask").data
+
+
+def save_results_callback(infer_request: ov.InferRequest, data) -> None:
+    anomaly_map = infer_request.get_tensor("anomaly_map").data
+    pred_mask = infer_request.get_tensor("pred_mask").data
+    data.anomaly_map=torch.tensor(anomaly_map.squeeze())
+    data.pred_mask=torch.tensor(pred_mask.squeeze())
+
+    result_map = visualize_image_item(
+        data,
+        fields=["image", "anomaly_map"],
+        fields_config={
+            "anomaly_map": {"colormap": True, "normalize": True}
+        }
+    )
+    base_img_path = "".join(data.image_path.split('/')[-2:])
+    amap_path = "res/anomaly_map/"+ base_img_path
+    overlay_path = "res/overlay/" + base_img_path
+    result_map.save(amap_path)
+    result_overlay = visualize_image_item(
+        data,
+        overlay_fields=[("image", ["gt_mask", "pred_mask"])],
+        overlay_fields_config={
+            "gt_mask": {"mode": "contour", "color": (0, 255, 0), "alpha": 0.7},
+            "pred_mask": {"mode": "contour", "color": (255, 0, 0), "alpha": 0.7}
+        }
+    )
+    result_overlay.save(overlay_path)
+
+
+def main():
+    log.basicConfig(format='[ %(levelname)s ] %(message)s', level=log.INFO, stream=sys.stdout)
+    log.info('OpenVINO:')
+    log.info(f"{'Build ':.<39} {ov.__version__}")
+    args = arg_parser()
+
+    device_name = args.device
+    
+    os.makedirs("res/anomaly_map", exist_ok=True)
+    os.makedirs("res/overlay", exist_ok=True)
+
+    tput = {'PERFORMANCE_HINT': 'THROUGHPUT'}
+
+    core = ov.Core()
+    inmodel = core.read_model(args.model)
+    inmodel.reshape([1,3,256,256])
+    compiled_model = core.compile_model(inmodel, device_name, tput)
+
+    dataset = MVTecADDataset(
+        root="./datasets/MVTecAD",
+        category="bottle",
+        split="test"
+    )
+    infer_queue = ov.AsyncInferQueue(compiled_model)
+    infer_queue.set_callback(completion_callback)
+    resize_transform = transforms.Resize((256, 256))
+
+    print("\nStarting Performance Testing...")
+    stime = time.time()
+    for data in dataset:
+        resize_img = resize_transform(data.image)
+        nimg = np.expand_dims(resize_img, axis=0)
+        input_tensor = ov.Tensor(array=nimg, shared_memory=True)
+        infer_queue.start_async({0: input_tensor}, data)
+    infer_queue.wait_all()
+
+    dtime = time.time() - stime
+    print(f'\nPerformance:\n - {len(dataset)} images task {dtime:.2f} sec, {dtime/len(dataset):.3f} sec/image')
+    print(f' - throughput {len(dataset)/dtime:.2f} FPS')
+
+    print("\nSaving the results...")
+    sinfer_queue = ov.AsyncInferQueue(compiled_model)
+    sinfer_queue.set_callback(save_results_callback)
+
+    stime = time.time()
+    for data in dataset:
+        resize_transform = transforms.Resize((256, 256))
+        resize_img = resize_transform(data.image)
+        nimg = np.expand_dims(resize_img, axis=0)
+        input_tensor = ov.Tensor(array=nimg, shared_memory=True)
+        sinfer_queue.start_async({0: input_tensor}, data)
+    sinfer_queue.wait_all()
+
+    dtime = time.time() - stime
+    print(f'\nSaving Results Performance:\n - {len(dataset)} images task {dtime:.2f} sec, {dtime/len(dataset):.3f} sec/image')
+    print(f' - throughput {len(dataset)/dtime:.2f} FPS')
+
+
+
+
+if __name__ == '__main__':
+    main()
